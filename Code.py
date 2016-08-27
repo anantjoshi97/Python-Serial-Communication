9579b6eed15e66a2592a86f4e3d6c6ff56f73b60
anantjoshi97 <anantjoshi97.iitb@gmail.com>
2016-08-27 16:15:18 +0530

    Added input to terminal

--- Code.py
+++ Code.py
@@ -1,5 +1,7 @@
 import datetime 
 import serial
+import subprocess
+
 ser = serial.Serial('/dev/ttyACM1' , 9600)
 #ser = serial.Serial('/dev/tty.usbserial', 9600) 
 #ser = serial.Serial('/dev/ttyUSBx-1')
@@ -12,3 +14,12 @@ while True:
     		print('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
     	else:
         	continue
+#first try 
+"""        	
+process = subprocess.Popen([], stdout=PIPE, stderr=PIPE, universal_newlines=TRUE)
+stdout, stderr = subprocess.process.communicate()
+"""
+#second try
+"""
+output = subprocess.check_output([])
+"""
