cb2d804031d221a9a5a1a71c5e87be902a352291
anantjoshi97 <anantjoshi97.iitb@gmail.com>
2016-09-02 00:09:54 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -2,37 +2,60 @@ import datetime
 import serial
 import subprocess
 
+def predict(s): 
+	""" date/time in Unix format, the date and time in ASCII
+       (UTC), the elevation of the satellite in degrees, the  azimuth  of  the
+       satellite  in degrees, the orbital phase (modulo 256), the latitude (N)
+       and longitude (W) of the satellite's  sub-satellite  point,  the  slant
+       range  to  the  satellite  (in  kilometers),  the orbit number, and the
+       spacecraft's sunlight visibility information.  For example:  1003611710
+       Sat  20Oct01  21:01:50    11     6   164    51   72   1389  16669 * """
+       
+       ele = ''
+       lat = ''
+       lon = ''
+       i = 0 #iterator
+       qty_count = 0 #if this variable has value i, it means the ith quantity is being accessed
+
+       while (qty_count < 10) :
+       	if(s[i]==' '):
+       		i = i + 1
+       		continue
+       	if(i>0 and s[i-1]==' '):
+       		qty_count = qty_count + 1
+       	if(qty_count == 5):
+       		ele = ele + s[i]
+       	if(qty_count == 8):
+       		lat = lat + s[i]
+       	if(qty_count == 9): #longitude
+       		lon = lon + s[i]
+
+       	return ele , lat , lon
+       	.
+ 
 ser = serial.Serial('/dev/ttyACM1' , 9600)
-#ser = serial.Serial('/dev/tty.usbserial', 9600) 
-#ser = serial.Serial('/dev/ttyUSBx-1')
-#ser = serial.Serial('/dev/ttySx-1')
-#ser = serial.Serial('/dev/ttyCOMx') x = number after COM in Arduino IDE
+
+
+PIPE = subprocess.PIPE
 while True:
 	i = ser.inWaiting()
 	if(i>0):
-		with open('test1','ab') as f:
-			f.write(ser.readline())
-			f.write('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
-    		#print ser.readline()
-    		#print('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
+		with open('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()),'a+') as f:
+
+			f.write('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' ')
+
+			if(ser.readline() == '*'):
+				f.write(ser.readline() + " ") # star voltage written
+			if(ser.readline() == "#"):
+				f.write(ser.readline() + " ") # hash voltage written
+
+			process = subprocess.Popen(["predict", "-f" , "<satname>"],stdout=PIPE, stderr=PIPE, universal_newlines=0) 
+			stdout, stderr = process.communicate()
+			ele , lat , lon = predict(stdout)
+			f.write(ele + " ")
+			f.write(lat + " ")
+			f.write(lon + '\n')
+    		
     	else:
         	continue
         	
-#first try , successful
- 
-PIPE = subprocess.PIPE
-process = subprocess.Popen(["predict", "-f" , "<satname>"],stdout=PIPE, stderr=PIPE, universal_newlines=0) 
-stdout, stderr = process.communicate()
-
-print stdout
-if len(stderr)>0 :
-	print "error" , stderr
-
-#second try
-"""
-output = subprocess.check_output([])
-"""
-#third try
-"""
-output = subprocess.check_output([], universal_newlines = TRUE)
-"""
